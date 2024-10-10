edafdbd32c6e13edddfc90fe0a0ddbd3fecd1a50
ericgalbarn <ericgalbarn@gmail.com>
2024-10-10 22:04:34 +0700

    Completing Search Function For The Search Bar

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-
-const sendButton = document.querySelector('.btn-outline-secondary');
+const sendButton = document.querySelector(".btn-outline-secondary");
 
 // Thực hiện sự kiện khi người dùng click vào nút
-sendButton.addEventListener('click', function() {
-    alert('Button clicked!');
+sendButton.addEventListener("click", function () {
+  alert("Button clicked!");
 });
-
