cd09001f3805e968f188d5aa7945b0bcc821fbf6
Nguyen Van Giang <giangnv.22bi13132@usth.edu.vn>
2024-10-18 14:36:16 +0700

    Revert "Intial Frontend Setup For Blogging Platform Project"
    This reverts commit ecd4caf9a66c9b3c8094f83bb8a9e41f3dcd8539.
    test

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-const sendButton = document.querySelector('.btn-outline-secondary');
-
-// Thực hiện sự kiện khi người dùng click vào nút
-sendButton.addEventListener('click', function() {
-    alert('Button clicked!');
-});
-
